3757e4c9dbfdf5a08d589095cb429b0c2dace038
tomstry <stryjak.tomasz02@gmail.com>
2022-10-11 22:41:48 +0200

    essa

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,8 +1,8 @@
 def sum(arg1,arg2):
-    return arg1 + arg2
+    return float(arg1) + float(arg2)
 
 
 if __name__ == '__main__':
     print('suma = ' + str(sum(2,2)))
 
-print("__name__ = "+ __name__)
+#print("__name__ = "+ __name__)
