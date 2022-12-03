f0b3d0728f67686ec1e2d73f3cb4cc0bd591c3b8
Tomasz Stryjak <stryjak.tomasz@gmail.com>
2022-12-03 21:12:36 +0100

    zad2 completed

--- lab_8_1/script2.js
+++ lab_8_1/script2.js
@@ -30,11 +30,15 @@ function suma(str){
     }
     return sumall;
 }
+let napis = '';
 while(true){
-    let msg = window.prompt('podaj input');
+    let msg = window.prompt(napis);
     let numbers = cyfry(msg);
     let letters = litery(msg);
     let sum = suma(msg);
+    napis += (msg + '\n');
+    napis += (numbers + ' ' + letters + ' '+ sum
+    + '\n');
     document.getElementById('txt').innerText += (msg + '\n');
     document.getElementById('txt').innerText += (numbers + ' ' + letters + ' '+ sum
     + '\n');
