f023a85046e84ed21b4926903285bbedf2188ac8
Tomasz Stryjak <stryjak.tomasz@gmail.com>
2022-12-04 12:14:02 +0100

    tests done

--- lab_8_1/script2.js
+++ lab_8_1/script2.js
@@ -31,15 +31,15 @@ function suma(str){
     return sumall;
 }
 let napis = '';
-while(true){
-    let msg = window.prompt(napis);
-    let numbers = cyfry(msg);
-    let letters = litery(msg);
-    let sum = suma(msg);
-    napis += (msg + '\n');
-    napis += (numbers + ' ' + letters + ' '+ sum
-    + '\n');
-    document.getElementById('txt').innerText += (msg + '\n');
-    document.getElementById('txt').innerText += (numbers + ' ' + letters + ' '+ sum
-    + '\n');
-}
+// while(true){
+//     let msg = window.prompt(napis);
+//     let numbers = cyfry(msg);
+//     let letters = litery(msg);
+//     let sum = suma(msg);
+//     napis += (msg + '\n');
+//     napis += (numbers + ' ' + letters + ' '+ sum
+//     + '\n');
+//     document.getElementById('txt').innerText += (msg + '\n');
+//     document.getElementById('txt').innerText += (numbers + ' ' + letters + ' '+ sum
+//     + '\n');
+// }
--- lab_8_1/test.js
+++ lab_8_1/test.js
@@ -1,14 +1,25 @@
 var expect = chai.expect;
 
-function sum(x,y) {
-	return x+y;
-}
-
-describe('The sum() function', function() {
- it('Returns 4 for 2+2', function() {
-   expect(sum(2,2)).to.equal(4);
+describe('Tests', function() {
+ it('Cyfry: ', function() {
+    chai.assert.equal(cyfry('1234'),10);
+    chai.assert.equal(cyfry('abcd'),0);
+    chai.assert.equal(cyfry('ab2137'),13);
+    chai.assert.equal(cyfry('17xd'),8);
+    chai.assert.equal(cyfry(''),0);
  });
- it('Returns 0 for -2+2', function() {
-   expect(sum(-2,2)).to.equal(0);
+ it('Litery: ', function() {
+    chai.assert.equal(litery('1234'),0);
+    chai.assert.equal(litery('abcd'),4);
+    chai.assert.equal(litery('ab2137'),2);
+    chai.assert.equal(litery('17xd'),2);
+    chai.assert.equal(litery(''),0);
+ });
+ it('Suma: ',function() {
+    chai.assert.equal(suma('1234'),1234);
+    chai.assert.equal(suma('abcd'),1234);
+    chai.assert.equal(suma('ab2137'),1234);
+    chai.assert.equal(suma('17xd'),1251);
+    chai.assert.equal(suma(''),1251);
  });
 });
